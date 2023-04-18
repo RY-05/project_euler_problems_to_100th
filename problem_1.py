582cd1123908bbd2f21f56023f0831bc2438c10c
RY <126905892+RY-05@users.noreply.github.com>
2023-04-18 20:57:57 +0100

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -1,22 +1,34 @@
-#   If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+#   If we list all the natural numbers below 10 
+#   that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 #   The sum of these multiples is 23.
 #   Find the sum of all the multiples of 3 and 5 below 1000.
 
 # //
 
 
-sum_int = 0
-for i in range(1, 10):
-    if (i % 3 == 0) or (i % 5 == 0):
-        # modulus operation helps check if the numbers that are ran through are multiples
-        sum_int = sum_int + i
-print(sum_int)
-# this returns 23, consistent with the question
+a = 3
+b = 5
+val1 = 10
+val2 = 1000
 
-sum_int = 0
-for i in range(1, 1000):
-    # end parameter for 'for loop' is exclusive, so all positive integers < 1000 are checked
-    if (i % 3 == 0) or (i % 5 == 0):
-        sum_int = sum_int + i
-print(sum_int)
-# this returns 233168, and thus is the answer
+
+def sum_multiples(num1, num2, limit):
+    total = 0
+    for i in range(1, limit):
+        # modulus operation helps check if the numbers
+        # that are ran through are multiples
+        if (i % num1 == 0) or (i % num2 == 0):
+            total += i
+
+    return total
+
+
+# this returns 23, which is consistent with the question
+print("The sum of all the multiples of " + str(a) + " and "\
+      + str(b) + " below " + str(val1) + " is "\
+      + str(sum_multiples(a, b, val1)) + ".")
+
+# this returns 233168, the correct answer
+print("The sum of all the multiples of " + str(a) + " and "\
+      + str(b) + " below " + str(val2) + " is "\
+      + str(sum_multiples(a, b, val2)) + ".")
