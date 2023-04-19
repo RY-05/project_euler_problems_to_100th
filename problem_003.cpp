f812a6a3ee33669da9c9eaaf9c4e600f3ba408fb
RY <126905892+RY-05@users.noreply.github.com>
2023-04-19 12:50:34 +0100

    Update problem_003.cpp

--- problem_003.cpp
+++ problem_003.cpp
@@ -1,62 +1,62 @@
-// The prime factors of 13195 are 5, 7, 13 and 29.
+//   The prime factors of 13195 are 5, 7, 13 and 29.
+//   What is the largest prime factor of the number 600851475143 ?
 
-// What is the largest prime factor of the number 600851475143 ?
+#   //
 
-// #
-
-
-// Write program in C++
-// Because Python is too inefficient for extremely large values
+//  Re-write code in C++ because Python is too inefficient
 
 
 // Python code
 
 /*
-num = 13195
+import math as m
 
+num1 = 13195
+num2 = 600851475143
 
-# function to check if given value is prime
-def prime(val):
+def prime_bool(x):
+    # variable counts factors of x, besides 1 and x
+    fac_count = 2
 
-    # list for numbers that are factors of given value
-    divisibles = []
+    # check for factors between 2 and the square root
+    k = m.floor(m.sqrt(x))
+    for i in range(2, k + 1):
+        if x % i == 0:
+            fac_count += 1
 
-    # running through numbers from 1 to half of given value to check if each of them are a factor
-    # stop at half because it is impossible for a factor to be greater than a half of the given value
-    for number in range(1, ((val + 1) // 2) + 1):
-        if val % number == 0:
-            divisibles.append(number)
-
-    # evident that given value would be a factor of the given value
-    # skip through checking if values greater than half are factors, certainly not
-    divisibles.append(val)
-
-    # primes have only two factors: one and itself
-    if len(divisibles) == 2 and divisibles[-1] != 1:
+    # primes have no factors between 2 and their square root
+    if fac_count == 2 and x != 1:
         return True
+
     else:
         return False
 
 
-# generate list of factors of num
-factors = []
+def factors(x):
+    list = []
+    for i in range(1, (x // 2) + 1):
+        if x % i == 0:
+            list.append(i)
+    list.append(x)
 
-for i in range(1, (num + 1) // 2):
-    if num % i == 0:
-        factors.append(i)
-factors.append(num)
+    return list
 
-print(factors)
 
-# generate list of prime factors of num
-prime_factors = []
+prime_facs = []
+for i in factors(num1):
+    if prime_bool(i):
+        prime_facs.append(i)
 
-for j in factors:
-    if prime(j):
-        prime_factors.append(j)
+# this returns 5, 7, 13 and 29, which is consistent with the question
+print("The prime factors of " + str(num1) + " are " + str(prime_facs) + ".")
+print("The largest prime factor of " + str(num1) + " is " + str(max(prime_facs)) + ".")
 
-print(prime_factors)
 
-# print largest prime factor of num
-print(max(prime_factors))
+prime_facs = []
+for i in factors(num2):
+    if prime_bool(i):
+        prime_facs.append(i)
+
+print("The prime factors of " + str(num1) + " are " + str(prime_facs) + ".")
+print("The largest prime factor of " + str(num2) + " is " + str(max(prime_facs)) + ".")
 */
