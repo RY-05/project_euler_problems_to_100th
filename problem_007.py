c31198a88ba0589a70bb55b3277081e19ad55aca
RY <126905892+RY-05@users.noreply.github.com>
2023-04-23 17:04:44 +0100

    Update problem_007.py

--- problem_007.py
+++ problem_007.py
@@ -1,10 +1,3 @@
-# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-
-# What is the 10,001st prime number?
-
-# /
-
-
 import math as m
 
 # val = int(input("Enter n such that you receive the nth prime number."))
