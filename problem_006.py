ff40067099add87c555955ce978de376ccf21121
RY <126905892+RY-05@users.noreply.github.com>
2023-04-23 17:03:37 +0100

    Update problem_006.py

--- problem_006.py
+++ problem_006.py
@@ -1,4 +1,5 @@
-lim = 100
+lim1 = 10
+lim2 = 100
 
 
 # sum of squares, 1 to limit inclusive
@@ -11,6 +12,12 @@ def sqr_sum(a):
     return sum(i for i in range(1, a + 1)) ** 2
 
 
+# returns 2640, which is consistent with the question
+print(f"The difference between the sum of the squares and "
+      f"the square of the sum of all natural numbers up to "
+      f"{lim1} is {sqr_sum(lim1) - sum_sqr(lim1)}.")
+
 # returns 25164150, the correct answer
 print(f"The difference between the sum of the squares and "
-      f"the square of the sum is {sqr_sum(lim) - sum_sqr(lim)}.")
+      f"the square of the sum of all natural numbers up to "
+      f"{lim2} is {sqr_sum(lim2) - sum_sqr(lim2)}.")
