2559a66f8799e3931ee19aee0ed34e9ebcefc589
RY <126905892+RY-05@users.noreply.github.com>
2023-04-23 15:08:21 +0100

    Update problem_001.py

--- problem_001.py
+++ problem_001.py
@@ -5,22 +5,12 @@ val2 = 1000
 
 
 def sum_multiples(num1, num2, limit):
-    total = 0
-    for i in range(1, limit):
-        # modulus operation helps check if the numbers
-        # that are ran through are multiples
-        if (i % num1 == 0) or (i % num2 == 0):
-            total += i
-
-    return total
+    return sum([i for i in range(1, limit) if (i % num1 == 0) or (i % num2 == 0)])
 
 
 # this returns 23, which is consistent with the question
-print("The sum of all the multiples of " + str(a) + " and "\
-      + str(b) + " below " + str(val1) + " is "\
-      + str(sum_multiples(a, b, val1)) + ".")
+print(f"The sum of all the multiples of {a} and {b} below {val1} is {sum_multiples(a, b, val1)}.")
 
 # this returns 233168, the correct answer
-print("The sum of all the multiples of " + str(a) + " and "\
-      + str(b) + " below " + str(val2) + " is "\
-      + str(sum_multiples(a, b, val2)) + ".")
+print(f"The sum of all the multiples of {a} and {b} below {val2} is {sum_multiples(a, b, val2)}.")
+
